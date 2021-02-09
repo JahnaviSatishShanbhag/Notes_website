b67ee855985d5c3647b7a4c3028d89677ba17a59
JahnaviSatishShanbhag <jahnavisatish.cs19@bmsce.ac.in>
2021-02-09 17:02:05 +0530

    sixth commit

--- JavaScript/notes_page.js
+++ JavaScript/notes_page.js
@@ -1,6 +1,6 @@
-let username=localStorage.getItem('username');
+let username = localStorage.getItem('username');
 window.onload = function () {
-    fetch('http://localhost:5500/users/'+username)
+    fetch('http://localhost:5500/users/' + username)
         .then((response) => response.json())
         .then((data) => {
             loadNotes(data);
@@ -20,7 +20,8 @@ function loadNotes(data) {
                     <div class="card-body">
                         <h5 class="card-title">${element.title}</h5>
                         <p class="card-text"> ${element.note}</p>
-                        <button class="btn btn-primary">Delete Note</button>
+                        <button data-id="${element.id}" class="btn btn-primary" onclick="editNote(this)">Edit Note</button>
+                        <button data-id="${element.id}" class="btn btn-primary" onclick="deleteNote(this)">Delete Note</button>
                     </div>
                 </div>`;
         });
@@ -28,20 +29,95 @@ function loadNotes(data) {
     notes.innerHTML = html;
 }
 
-document.getElementById('addBtn').addEventListener('click',()=>
-{
-    let title=document.getElementById('addTitle').value;
-    let note=document.getElementById('addTxt').value;
-    fetch('http://localhost:5500/users/post/'+username,
-    {
-        headers:{
-            'Content-type':'application/json'
-        },
-        method:'POST',
-        body:JSON.stringify({title:title,note:note})
-    })
-    .then((response)=>response.json())
-    .then((data)=>{
-        location.reload();
-    });
+document.getElementById('addBtn').addEventListener('click', () => {
+    let title = document.getElementById('addTitle').value;
+    let note = document.getElementById('addTxt').value;
+    if (title.trim() == "" && note.trim() == "") {
+        document.getElementById('addTitle').style.border = "2px solid red";
+        document.getElementById('addTxt').style.border = "2px solid red";
+        document.getElementById('label').style.display = 'inline';
+        return false;
+    }
+    if (title.trim() == "") {
+        document.getElementById('addTitle').style.border = "2px solid red";
+        document.getElementById('addTxt').style.border = "1px solid gray";
+        document.getElementById('label').style.display = 'inline';
+        return false;
+    }
+    if (note.trim() == "") {
+        document.getElementById('addTxt').style.border = "2px solid red";
+        document.getElementById('addTitle').style.border = "1px solid gray";
+        document.getElementById('label').style.display = 'inline';
+        return false;
+    }
+    fetch('http://localhost:5500/users/post/' + username,
+        {
+            headers: {
+                'Content-type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify({ title: title, note: note })
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            location.reload();
+        });
 });
+
+function editNote(element) {
+    let title = document.getElementById('addTitle');
+    let note = document.getElementById('addTxt');
+    let id = parseInt(element.getAttribute('data-id'));
+    fetch('http://localhost:5500/users/' + username + '/' + id)
+        .then((response) => response.json())
+        .then((data) => {
+            title.value = data[0].title;
+            note.value = data[0].note;
+        });
+    document.getElementById('addBtn').style.display = 'none';
+    document.getElementById('editBtn').style.display = 'inline';
+    document.getElementById('editBtn').addEventListener('click', () => {
+        if (title.value.trim() == "" && note.value.trim() == "") {
+            document.getElementById('addTitle').style.border = "2px solid red";
+            document.getElementById('addTxt').style.border = "2px solid red";
+            document.getElementById('label').style.display = 'inline';
+            return false;
+        }
+        if (title.value.trim() == "") {
+            document.getElementById('addTitle').style.border = "2px solid red";
+            document.getElementById('addTxt').style.border = "1px solid gray";
+            document.getElementById('label').style.display = 'inline';
+            return false;
+        }
+        if (note.value.trim() == "") {
+            document.getElementById('addTxt').style.border = "2px solid red";
+            document.getElementById('addTitle').style.border = "1px solid gray";
+            document.getElementById('label').style.display = 'inline';
+            return false;
+        }
+        fetch('http://localhost:5500/users/edit/' + username + '/' + id, {
+            headers: {
+                'Content-type': 'application/json'
+            },
+            method: 'PATCH',
+            body: JSON.stringify({ title: title.value, note: note.value })
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                document.getElementById('editBtn').style.display = 'none';
+                document.getElementById('addBtn').style.display = 'inline';
+                location.reload();
+            });
+    });
+}
+
+function deleteNote(element) {
+    let id = parseInt(element.getAttribute('data-id'));
+    fetch('http://localhost:5500/users/delete/' + username + '/' + id, {
+        method: 'DELETE',
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            location.reload();
+        });
+}
--- Server/app.js
+++ Server/app.js
@@ -1,121 +1,150 @@
-let express=require('express');
-let mysql=require('mysql');
-let app=express();
-let cors=require('cors');
-let dotenv=require('dotenv');
+let express = require('express');
+let mysql = require('mysql');
+let app = express();
+let cors = require('cors');
+let dotenv = require('dotenv');
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-const connection=mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USERNAME,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DATABASE,
-    port:process.env.PORTS
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORTS
 });
 
-connection.connect((error)=>
-{
-    if (error)
-    {
+connection.connect((error) => {
+    if (error) {
         console.log(error.message);
     }
-    else
-    {
+    else {
         console.log("database connected");
     }
 });
 
-app.get('/getData',(req,res)=>
-{
-    const sql='SELECT * FROM users;';
-    const query=connection.query(sql,(error,results)=>
-    {
-        if (error)
-        {
+app.get('/getData', (req, res) => {
+    const sql = 'SELECT * FROM users;';
+    const query = connection.query(sql, (error, results) => {
+        if (error) {
             throw error;
         }
-        else
-        {
+        else {
             res.send(results);
         }
     });
 });
 
-app.post('/postData',(req,res)=>
-{
-    const {name}=req.body;
-    const {email}=req.body;
-    const {password}=req.body;
-    const sql='INSERT INTO users(name,email,password) VALUES(?,?,?);';
-    const query=connection.query(sql,[name,email,password],(error,result)=>
-    {
-        if (error)
-        {
+app.post('/postData', (req, res) => {
+    const { name } = req.body;
+    const { email } = req.body;
+    const { password } = req.body;
+    const sql = 'INSERT INTO users(name,email,password) VALUES(?,?,?);';
+    const query = connection.query(sql, [name, email, password], (error, result) => {
+        if (error) {
             throw error;
         }
-        else
-        {
-            let table_sql="CREATE TABLE "+name+"(id INT AUTO_INCREMENT PRIMARY KEY,title VARCHAR(100) NOT NULL,note VARCHAR(15900) NOT NULL);";
-            let table_query=connection.query(table_sql,(error,reslt)=>
-            {
+        else {
+            let table_sql = "CREATE TABLE " + name + "(id INT AUTO_INCREMENT PRIMARY KEY,title VARCHAR(100) NOT NULL,note VARCHAR(15900) NOT NULL);";
+            let table_query = connection.query(table_sql, (error, reslt) => {
                 if (error) throw error;
             });
-            const postData=
+            const postData =
             {
-                id:result.insertId,
-                name:name,
-                email:email,
-                password:password
+                id: result.insertId,
+                name: name,
+                email: email,
+                password: password
             };
             res.send(postData);
         }
     });
 });
 
-app.get('/users/:username',(req,res)=>
-{
-    let username=req.params.username;
-    const sql='SELECT * FROM '+username;
-    const query=connection.query(sql,(error,results)=>
-    {
-        if (error)
-        {
+app.get('/users/:username', (req, res) => {
+    let username = req.params.username;
+    const sql = 'SELECT * FROM ' + username;
+    const query = connection.query(sql, (error, results) => {
+        if (error) {
             throw error;
         }
-        else
-        {
+        else {
             res.send(results);
         }
     });
 });
 
-app.post('/users/post/:username',(req,res)=>
-{
-    let username=req.params.username;
-    let {title}=req.body;
-    let {note}=req.body;
-    const sql=`INSERT INTO ${username}(title,note) VALUES(?,?);`;
-    const query=connection.query(sql,[title,note],(error,result)=>
-    {
-        if (error)
-        {
+app.get('/users/:username/:id', (req, res) => {
+    let username = req.params.username;
+    let id = req.params.id;
+    const sql = 'SELECT * FROM ' + username + ' WHERE id=' + id;
+    const query = connection.query(sql, (error, result) => {
+        if (error) {
             throw error;
         }
-        else
-        {
-            const postNote=
+        else {
+            res.send(result);
+        }
+    });
+});
+
+app.post('/users/post/:username', (req, res) => {
+    let username = req.params.username;
+    let { title } = req.body;
+    let { note } = req.body;
+    const sql = `INSERT INTO ${username}(title,note) VALUES(?,?);`;
+    const query = connection.query(sql, [title, note], (error, result) => {
+        if (error) {
+            throw error;
+        }
+        else {
+            const postNote =
             {
-                id:result.insertId,
-                title:title,
-                note:note
+                id: result.insertId,
+                title: title,
+                note: note
             };
             res.send(postNote);
         }
     });
 });
 
-app.listen(process.env.PORT,console.log("Listening to the port "+process.env.PORT));
+app.patch('/users/edit/:username/:id', (req, res) => {
+    let username = req.params.username;
+    let id = req.params.id;
+    let { title } = req.body;
+    let { note } = req.body;
+    const sql = "UPDATE " + username + " SET title='" + title + "', note='" + note + "' WHERE id=" + id;
+    const query = connection.query(sql, (error, result) => {
+        if (error) {
+            throw error;
+        }
+        else {
+            const patchData =
+            {
+                title: title,
+                note: note
+            };
+            res.send(patchData)
+        }
+    });
+});
+
+app.delete('/users/delete/:username/:id', (req, res) => {
+    let username = req.params.username;
+    let id = req.params.id;
+    const sql = "DELETE FROM " + username + " WHERE id=" + id;
+    const query = connection.query(sql, (error, result) => {
+        if (error) {
+            throw error;
+        }
+        else {
+            res.send(result);
+        }
+    });
+});
+
+app.listen(process.env.PORT, console.log("Listening to the port " + process.env.PORT));
